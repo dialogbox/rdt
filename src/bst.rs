d17ed2f923bf59258e1435a680c39ad11bc80830
Jason Kim <dialogbox@gmail.com>
2018-10-30 07:27:43 +0900

    WIP BST

--- src/bst.rs
+++ src/bst.rs
@@ -1,29 +1,56 @@
 #[derive(Debug)]
-struct BST<T: PartialOrd> {
-    root: Option<&'a mut TreeNode<T>>,
+struct Data(u64, String);
+
+#[derive(Debug)]
+struct BST<K, V> {
+    root: NodeEntry<K, V>
 }
 
 #[derive(Debug)]
-struct TreeNode<'a, T: PartialOrd> {
-    value: T,
-    left: Option<&'a mut TreeNode<T>>,
-    right: Option<&'a mut TreeNode<T>>),
+enum NodeEntry<K, V> {
+    Node(K, V, Box<NodeEntry<K, V>>, Box<NodeEntry<K, V>>),
+    Terminal,
 }
 
-impl<T: PartialOrd> BST<T> {
-    fn add(&mut self, n: T) {}
+impl <K, V> BST<K, V> where K: PartialOrd {
+    pub fn new() -> BST<K, V> {
+        BST::<K, V> { root: NodeEntry::Terminal }
+    }
+
+    pub fn add(&mut self, k: K, v: V) {
+        self.root = NodeEntry::Node(k, v, Box::new(NodeEntry::Terminal), Box::new(NodeEntry::Terminal));
+    }
+
+    fn try_search<'a>(cur: &'a NodeEntry<K, V>, k: K) -> &'a NodeEntry<K, V> {
+        match cur {
+            NodeEntry::Terminal => &cur,
+            NodeEntry::Node(ref ck, _, _, _) if *ck == k => &cur,
+            NodeEntry::Node(ref ck, _, ref l, _) if *ck < k => BST::try_search(l, k),
+            NodeEntry::Node(_, _, _, ref r) => BST::try_search(r, k),
+        }
+    }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
+    use std::collections::BTreeMap;
 
     #[test]
     fn new_tree() {
-        let t = &BST::<i64> {
-            root: TreeNode::Teminal,
-        };
+        let mut movie_reviews = BTreeMap::new();
 
-        println!("{:?}", t);
+        movie_reviews.insert("Office Space", Data(10, "Deals with real issues in the workplace.".to_string()));
+        movie_reviews.insert("Pulp Fiction", Data(2, "Masterpiece.".to_string()));
+        movie_reviews.insert("The Godfather", Data(1, "Very enjoyable..".to_string()));
+        movie_reviews.insert("The Blues Brothers", Data(99, "Eye lyked it alot.".to_string()));
+
+        println!("{:?}", movie_reviews);
+
+        let mut bst = BST::<&str, Data>::new();
+
+        bst.add("Office Space", Data(10, "Deals with real issues in the workplace.".to_string()));
+
+        println!("{:?}", bst);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,1 @@
 mod bst;
-mod list;
