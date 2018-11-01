1d14213587a3c7f4d4b9fc95731575a655b3319d
Jason Kim <junchkim@ebay.com>
2018-11-01 13:37:12 +0900

    BST delete implemented

--- src/bst.rs
+++ src/bst.rs
@@ -38,32 +38,101 @@ impl <T: Ord> BST<T> {
         }
     }
 
-    // deletion entry point
+    fn pop_min(self) -> (BST<T>, Option<T>) {
+        match self {
+            BST::Teminal => (BST::Teminal, Option::None),
+            BST::Node(node) => {
+                let node = *node;
+                let TreeNode { v, l, r } = node;
+
+                match (r, l) {
+                    (BST::Teminal, BST::Teminal) => {
+                        (BST::Teminal, Option::Some(v))
+                    },
+                    (r @ BST::Node(_), BST::Teminal) => {
+                        (r, Option::Some(v))
+                    },
+                    (r @ _, l @ BST::Node(_)) => {
+                        let (l, minv) = l.pop_min();
+
+                        (BST::Node(Box::new(TreeNode{ v, l, r })), minv)
+                    },
+                }
+            }
+        }
+    }
+
+    fn pop_max(self) -> (BST<T>, Option<T>) {
+        match self {
+            BST::Teminal => (BST::Teminal, Option::None),
+            BST::Node(node) => {
+                let node = *node;
+                let TreeNode { v, l, r } = node;
+
+                match (l, r) {
+                    (BST::Teminal, BST::Teminal) => {
+                        (BST::Teminal, Option::Some(v))
+                    },
+                    (l @ BST::Node(_), BST::Teminal) => {
+                        (l, Option::Some(v))
+                    },
+                    (l @ _, r @ BST::Node(_)) => {
+                        let (r, maxv) = r.pop_max();
+
+                        (BST::Node(Box::new(TreeNode{ v, l, r })), maxv)
+                    },
+                }
+            }
+        }
+    }
+
     fn delete(self, value: T) -> BST<T> {
         match self {
             BST::Teminal => self,
             BST::Node(node) => {
                 if value > node.v {
-                    node.r.delete(value)
+                    // https://stackoverflow.com/a/28469639
+                    // Once nll is stabilized this could be simply like this
+                    // let TreeNode { v, l, r } = *node;
+                    let node = *node;
+                    let TreeNode { v, l, r } = node;
+
+                    let r = r.delete(value);
+                    BST::Node(Box::new(TreeNode{v, l, r}))
                 } else if value < node.v {
-                    node.l.delete(value)
+                    let node = *node;
+                    let TreeNode { v, l, r } = node;
+                    let l = l.delete(value);
+                    BST::Node(Box::new(TreeNode{v, l, r}))
                 } else {
+                    // https://en.wikipedia.org/wiki/Binary_search_tree#Deletion
+                    // T. Hibbard in 1962
                     let node = *node;
                     let TreeNode { v: _, l, r } = node;
 
                     match (l, r) {
                         (BST::Teminal, BST::Teminal) => BST::Teminal,
-                        (BST::Node(node), BST::Teminal) => BST::Node(node),
-                        (BST::Teminal, BST::Node(node)) => BST::Node(node),
-                        (BST::Node(lnode), BST::Node(rnode)) => {
-                            panic!("Not implemented!");
+                        (l @ BST::Node(_), BST::Teminal) => l,
+                        (BST::Teminal, r @ BST::Node(_)) => r,
+                        (l @ BST::Node(_), r @ BST::Node(_)) => {
+                            let (l, maxl) = l.pop_max();
+                            match maxl {
+                                Option::None => {
+                                    let (r, minr) = r.pop_min();
+                                    BST::Node(Box::new(TreeNode { v: minr.unwrap(), l, r }))
+                                },
+                                Option::Some(maxl) => {
+                                    BST::Node(Box::new(TreeNode { v: maxl, l, r }))
+                                }
+                            }
                         },
                     }
-
                 }
             },
         }
     }
+
+
 
 }
 
@@ -91,6 +160,100 @@ mod test {
 
         assert_eq!(bst.get("Pulp Fiction").is_none(), true);
         assert_eq!(bst.get("Pulp Fiction2").unwrap().v, "Pulp Fiction2");
+    }
+
+    #[test]
+    fn test_pop_min() {
+        let bst = BST::Teminal;
+
+        let (mut bst, min) = bst.pop_min();
+        assert_eq!(min, Option::None);
+
+        //       10
+        //      /  \
+        //     6   11
+        //    /
+        //   4
+        //  / \
+        // 2   5
+        //  \
+        //   3
+        bst.add(10);
+        bst.add(11);
+        bst.add(6);
+        bst.add(4);
+        bst.add(2);
+        bst.add(3);
+        bst.add(5);
+
+        let (bst, min) = bst.pop_min();
+        assert_eq!(min, Option::Some(2));
+        let (bst, min) = bst.pop_min();
+        assert_eq!(min, Option::Some(3));
+        let (bst, min) = bst.pop_min();
+        assert_eq!(min, Option::Some(4));
+        let (bst, min) = bst.pop_min();
+        assert_eq!(min, Option::Some(5));
+        let (bst, min) = bst.pop_min();
+        assert_eq!(min, Option::Some(6));
+        let (bst, min) = bst.pop_min();
+        assert_eq!(min, Option::Some(10));
+        let (bst, min) = bst.pop_min();
+        assert_eq!(min, Option::Some(11));
+        let (bst, min) = bst.pop_min();
+        assert_eq!(min, Option::None);
+
+        match bst {
+            BST::Node(_) => panic!("BST should be empty"),
+            _ => ()
+        }
+    }
+
+    #[test]
+    fn test_pop_max() {
+        let bst = BST::Teminal;
+
+        let (mut bst, max) = bst.pop_max();
+        assert_eq!(max, Option::None);
+
+        //       10
+        //      /  \
+        //     6   11
+        //    /
+        //   4
+        //  / \
+        // 2   5
+        //  \
+        //   3
+        bst.add(10);
+        bst.add(11);
+        bst.add(6);
+        bst.add(4);
+        bst.add(2);
+        bst.add(3);
+        bst.add(5);
+
+        let (bst, max) = bst.pop_max();
+        assert_eq!(max, Option::Some(11));
+        let (bst, max) = bst.pop_max();
+        assert_eq!(max, Option::Some(10));
+        let (bst, max) = bst.pop_max();
+        assert_eq!(max, Option::Some(6));
+        let (bst, max) = bst.pop_max();
+        assert_eq!(max, Option::Some(5));
+        let (bst, max) = bst.pop_max();
+        assert_eq!(max, Option::Some(4));
+        let (bst, max) = bst.pop_max();
+        assert_eq!(max, Option::Some(3));
+        let (bst, max) = bst.pop_max();
+        assert_eq!(max, Option::Some(2));
+        let (bst, max) = bst.pop_max();
+        assert_eq!(max, Option::None);
+
+        match bst {
+            BST::Node(_) => panic!("BST should be empty"),
+            _ => ()
+        }
     }
 
     #[test]
@@ -133,6 +296,29 @@ mod test {
         bst = bst.delete(4);
 
         assert_eq!(bst.get(4).is_none(), true);
+
+        let mut bst = BST::Teminal;
+
+        //       10
+        //      /  \
+        //     6   11
+        //    /
+        //   4
+        //  / \
+        // 2   5
+        //  \
+        //   3
+        bst.add(10);
+        bst.add(11);
+        bst.add(6);
+        bst.add(4);
+        bst.add(2);
+        bst.add(3);
+        bst.add(5);
+
+        bst = bst.delete(10);
+
+        assert_eq!(bst.get(10).is_none(), true);
 
         println!("{:#?}", bst);
     }
